805fc219da03455b460c0b3e2fce0469369d4cb4
Aditya Kulshrestha <adi.kul358@gmail.com>
2023-08-21 17:33:52 +0530

    Thumbnail update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,14 +41,14 @@ export default function RootLayout({
         <meta property="og:url" content="https://c6ab-103-4-221-252.ngrok-free.app/" />
         <meta property="og:title" content="Team Acta — Hack Sustain 2023" />
         <meta property="og:description" content="" />
-        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:image" content="/thumbnail.jpg" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://c6ab-103-4-221-252.ngrok-free.app/" />
         <meta property="twitter:title" content="Team Acta — Hack Sustain 2023" />
         <meta property="twitter:description" content="" />
-        <meta property="twitter:image" content="/thumbnail.png" />
+        <meta property="twitter:image" content="/thumbnail.jpg" />
         
         <Script src="https://kit.fontawesome.com/b0435a4746.js" crossOrigin="anonymous"></Script>
       </head>
