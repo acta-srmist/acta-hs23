fa5d48246c4061adbd82f61be59559702e211d19
Aditya Kulshrestha <adi.kul358@gmail.com>
2023-08-21 18:33:43 +0530

    HTML head fix 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,17 +40,17 @@ export default function RootLayout({
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://c6ab-103-4-221-252.ngrok-free.app/" />
+        <meta property="og:url" content="https://actahs23.adikul.dev/" />
         <meta property="og:title" content="Team Acta — Hack Sustain 2023" />
         <meta property="og:description" content="" />
-        <meta property="og:image" content="/thumbnail.jpg" />
+        <meta property="og:image" content="https://actahs23.adikul.dev/thumbnail.jpg" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://c6ab-103-4-221-252.ngrok-free.app/" />
+        <meta property="twitter:url" content="https://actahs23.adikul.dev/" />
         <meta property="twitter:title" content="Team Acta — Hack Sustain 2023" />
         <meta property="twitter:description" content="" />
-        <meta property="twitter:image" content="/thumbnail.jpg" />
+        <meta property="twitter:image" content="https://actahs23.adikul.dev/thumbnail.jpg" />
         
       </Head>
       <Script src="https://kit.fontawesome.com/b0435a4746.js" crossOrigin="anonymous"></Script>
