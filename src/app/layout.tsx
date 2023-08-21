1decef349ede650d7d476049d346c7ad257679c2
Aditya Kulshrestha <adi.kul358@gmail.com>
2023-08-21 18:13:05 +0530

    Need to read what I am ctrl-c-ctrl-v-ing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <title>Team Acta — Hack Sustain 2023</title>
         <meta name="title" content="Team Acta — Hack Sustain 2023" />
         <meta name="description" content="" />
-        <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
